1260255a223c00519ba5cfa9e50618b520d144cc
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-09-22 09:42:50 -0300

    first commit

---
+++ veiculos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <title>Cadastro Veículos</title>
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="js/script.js" type="text/javascript"></script>  
+
+</head>
+
+<body>
+
+	<form action="veiculo.php" method="GET" name="cadastroVeiculoForm">
+
+		<table>
+			<tr>
+				<th>Marca :</th>
+				<td><input name="marca" type="text" id="marca" value=""/></td>
+			</tr>
+		</table>
+
+	</form>
+
+</body>
+
+</html>
