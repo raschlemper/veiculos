081d6d5061cf25b83c9366ef10ed2bc2756718de
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-09-22 09:46:02 -0300

    add

--- veiculos.php
+++ veiculos.php
@@ -1,27 +1,48 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
     <title>Cadastro Veículos</title>
 
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
     <script src="js/script.js" type="text/javascript"></script>  
 
 </head>
 
 <body>
 
-	<form action="veiculo.php" method="GET" name="cadastroVeiculoForm">
+	<div class="content">
 
-		<table>
-			<tr>
-				<th>Marca :</th>
-				<td><input name="marca" type="text" id="marca" value=""/></td>
-			</tr>
-		</table>
+		<div class="title">
+			<h3>Cadastro Veículos</h3>
+		</div>	
 
-	</form>
+		<div class="body">
+
+			<form action="veiculo.php" method="GET" name="cadastroVeiculoForm">
+
+				<div class="form-group">
+                    <label class="col-md-2 control-label" for="gender">Marca:</label>
+                    <div class="col-md-3">
+                        <select class="form-control" name="gender" id="marca" name="marca">
+                            <option value="0">Selecione item</option>
+                            <option value="1">Chevrolet</option>
+                        </select>
+                    </div>
+                </div>
+
+				<!-- <table>
+					<tr>
+						<th class="label">Marca :</th>
+						<td><input name="marca" type="text" id="marca" value=""/></td>
+					</tr>
+				</table> -->
+
+			</form>
+
+		</div>	
+
+	</div>	
 
 </body>
 
